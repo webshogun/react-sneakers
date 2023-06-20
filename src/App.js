4b4768726b33704175e877baba41f7684aabe816
Bohdan Barulin <webshogun@proton.me>
2023-06-20 13:47:28 +0300

    Connected mockapi.io and added the Card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { useState, useEffect } from 'react';
+import Card from './components/card';
 import Header from './components/header';
 
 export default function App() {
+	const [sneakers, setSneakers] = useState([]);
+
+	useEffect(() => {
+		fetch('https://649178b12f2c7ee6c2c84676.mockapi.io/sneakers')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setSneakers(data);
+			});
+	}, []);
+
 	return (
 		<div className='bg-blue-100 py-20'>
-			<div className='container mx-auto h-screen rounded-xl bg-white shadow-xl'>
+			<div className='max-w-[1080px] mx-auto rounded-xl bg-white shadow-xl'>
 				<Header />
+				<main className='px-14'>
+					<div className='flex justify-between items-center'>
+						<h1 className='font-inter font-bold text-3xl'>All sneakers</h1>
+						<div className='flex items-center gap-3 px-4 py-2 border rounded-lg'>
+							<img src='/images/search.svg' alt='Search' />
+							<input className='' placeholder='Search...' />
+						</div>
+					</div>
+					<div className='flex flex-wrap gap-10 py-6 justify-center'>
+						{sneakers?.map((item) => (
+							<Card key={item.id} img={item.id} price={item.price} name={item.name} />
+						))}
+					</div>
+				</main>
 			</div>
 		</div>
 	);
---
+++ src/components/card.js
@@ -0,0 +1,19 @@
+const Card = ({ img, name, price }) => {
+	return (
+		<div className=' border rounded-3xl p-4 w-[210px] h-[260px]'>
+			<img className='mx-auto' width={133} height={112} src={`/images/sneakers/${img}.jpg`} alt='Sneakers' />
+			<h5 className='py-2'>{name}</h5>
+			<div className='flex items-center justify-between'>
+				<div className='flex flex-col'>
+					<span>Price:</span>
+					<b>{price} $</b>
+				</div>
+				<button className='flex items-center justify-center h-8 w-8 border rounded-lg'>
+					<img width={11} height={11} src='/images/plus.svg' alt='Plus' />
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
